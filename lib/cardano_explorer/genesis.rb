408d16e3feaac5fd87078c68f6f57aa3c8aad856
Carlos Beltran <imthatcarlos@gmail.com>
2018-01-13 21:31:15 -0600

    comment #pages as it currently returns 404

--- lib/cardano_explorer/genesis.rb
+++ lib/cardano_explorer/genesis.rb
@@ -10,11 +10,12 @@ module CardanoExplorer
 
     # NOTE: defined in the API docs, but returns 404
     # https://cardanodocs.com/technical/explorer/api/#path--api-genesis-address-pages
-    def pages(options = {})
-      valid_params = %w[page pageSize redeemeed]
-      options = options.keep_if { |k| valid_params.include?(k) }
-      get('address/pages', options).parsed_response['Right']
-    end
+    #
+    # def pages(options = {})
+    #   valid_params = %w[page pageSize redeemeed]
+    #   options = options.keep_if { |k| valid_params.include?(k) }
+    #   get('address/pages', options).parsed_response['Right']
+    # end
 
     def pages_total(options = {})
       valid_params = %w[pageSize redeemeed]
