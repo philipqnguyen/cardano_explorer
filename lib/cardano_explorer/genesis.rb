8f71fabd53adf309576b22335591dee2d6aed096
Carlos Beltran <imthatcarlos@gmail.com>
2018-01-15 13:57:36 -0600

    param for Base constructor is optional, remove #pages from Genesis

--- lib/cardano_explorer/genesis.rb
+++ lib/cardano_explorer/genesis.rb
@@ -5,18 +5,6 @@ module CardanoExplorer
       raise NoMethodError
     end
 
-    def initialize
-    end
-
-    # NOTE: defined in the API docs, but returns 404
-    # https://cardanodocs.com/technical/explorer/api/#path--api-genesis-address-pages
-    #
-    # def pages(options = {})
-    #   valid_params = %w[page pageSize redeemeed]
-    #   options = options.keep_if { |k| valid_params.include?(k) }
-    #   get('address/pages', options).parsed_response['Right']
-    # end
-
     def pages_total(options = {})
       valid_params = %w[pageSize redeemeed]
       options = options.keep_if { |k| valid_params.include?(k) }
